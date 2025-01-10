1e7f5e15a1c4e36af54b09b2373943c8805d99d5
Eliminator746 <90308790+Eliminator746@users.noreply.github.com>
2025-01-10 11:03:27 +0530

    util added

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 import dotenv from 'dotenv';
 import connectDB from './src/db/index.js';
 
+
 dotenv.config({
-    path: './.env'
-})
+  path: './.env',
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on('error', (err) => {
+      console.error('Error: ' + err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Error: ' + err);
+    throw err;
+  });
 
-
-
-
-
+  
 
 // (async () => {
 //   try {
